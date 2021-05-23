a00c338dc1d2706eb469c4c35047a663532a6a81
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-05-23 19:47:44 +0530

    save

--- src/contexts/Quiz-Context.tsx
+++ src/contexts/Quiz-Context.tsx
@@ -58,7 +58,7 @@ type ActionType =
   | { type: "WRONG_ANS"; payload: { score: number } }
   | { type: "TOGGLE_DISABLE" };
 
-export const QuizContext = createContext<CxtState>({} as CxtState);
+export const QuizContext = createContext<CxtState | null>(null);
 
 export function redcFunc(
   redcState: initialStateType,
